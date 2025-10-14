0d25d19d0256209b99505984831dd2f73f88d078
Hao Feng <haofeng.ching@gmail.com>
2025-10-14 16:05:41 +0800

    tutorial
    i know how to use git

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ from helpers import *
 # within each sch segregate by gender, list, put all these lists in list called 'buckets'
 # these groups are basically as distinct as can be from each other but within each bucket, they only differ by cgpa
 
+#i did this work
