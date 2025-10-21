054c046520a4fd90653c6f1da4b2924f7d893743
Aniruddha Joshi <137318388+papiani@users.noreply.github.com>
2025-10-21 15:31:42 +0800

    Refactor data loading and segregation logic

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from helpers import *
 
 # read csv
+raw_data = load_raw_data("records.csv") # in format TutorialGroup, StudentID ,School, Name ,Gender ,CGPA
 
-# segregate by school, list for ach sch
+# segregate by school, list for each sch
+dictbysch = segregate(raw_data,2)
+
+    
 
 # within each sch segregate by gender, list, put all these lists in list called 'buckets'
 # these groups are basically as distinct as can be from each other but within each bucket, they only differ by cgpa
